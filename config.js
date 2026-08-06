59aabe707186830e38773191a8c6547616eacbcc
rikislow123 <rikisuparman754@gmail.com>
2026-08-07 01:20:32 +0800

    update

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // Firebase Initialization & Sync Engine
 const configObj = {
-    apiKey: "AIzaSyB9E39QtuLEMqFi6l9hEpIq0dbj0Cub8o4",
-    authDomain: "latihan-e6e0e.firebaseapp.com",
-    databaseURL: "https://latihan-e6e0e-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "latihan-e6e0e",
-    storageBucket: "latihan-e6e0e.firebasestorage.app",
-    messagingSenderId: "206077007630",
-    appId: "1:206077007630:web:95923c3dc84e2fd92c695f",
-    measurementId: "G-M522KHJD4Q"
+    apiKey: "AIzaSyBv_s-mc57UsYyqMYTDtVTtIWicJ_qn4hE",
+    authDomain: "monitoringsales-fec7a.firebaseapp.com",
+    databaseURL: "https://monitoringsales-fec7a-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "monitoringsales-fec7a",
+    storageBucket: "monitoringsales-fec7a.firebasestorage.app",
+    messagingSenderId: "767686039817",
+    appId: "1:767686039817:web:b07b31af6660ce7d1a9bd8",
+    measurementId: "G-JMDZMCZNPZ"
 };
 
 // Langsung inisialisasi agar jalan otomatis di semua halaman
